d4399f538c08cb517988e80c7c833131a56f1af5
Maksym Kulin <azwart111@gmail.com>
2023-01-04 16:54:13 +0200

    Create structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BlogComponent } from './blog/blog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostComponent } from './blog/post/post.component';
+import { ThumbComponent } from './blog/thumb/thumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BlogComponent,
+    PostComponent,
+    ThumbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/blog/thumb/thumb.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thumb',
+  templateUrl: './thumb.component.html',
+  styleUrls: ['./thumb.component.scss']
+})
+export class ThumbComponent {
+
+}
